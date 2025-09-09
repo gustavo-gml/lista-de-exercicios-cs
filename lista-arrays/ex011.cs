0d9d6f4e455ebd811d8c8aa875286dda024468af
gustavo-gml <gustavomartinslima06@gmail.com>
2025-09-09 12:37:15 -0300

    Melhoria da performance do exercício 11

--- lista-arrays/ex011.cs
+++ lista-arrays/ex011.cs
@@ -5,9 +5,6 @@ class ex011
 {
     static string DecodificarP(string mensagemCod)
     {
-        mensagemCod = mensagemCod.ToLower(); /*Garante o funcianamento das condicionais*/
-
-        char[] decodificacao = mensagemCod.ToCharArray(); /*transforma a string de entrada em um array do tipo char para possibilitar os comandos de decisão*/
 
         char[] decodificado = new char[mensagemCod.Length]; /*Vai armazenar a mensagem decodificada*/
 
@@ -15,22 +12,22 @@ class ex011
 
         for (int i = 1; i < mensagemCod.Length - 1; i++) /*A primeira letra sempre será um p, para todos os casos , o que faz com que não seja necessário verificar se esse caractere precisa ser testado. Além disso, todo final de palavra tem um caractere da palavra, portanto o ultimo caractere da string sempre será um caractere que precisa ser incluído, não necessitando de testes. Devido a isso, int i = 1; i < mensagemCod.Length - 1*/
         {
-            if (decodificacao[i - 1] == 'p' && (decodificacao[i + 1] == 'p' || decodificacao[i + 1] == ' '))  /*Satizfaz a decodificação para qualquer caracter não Vazio*/
+            if (mensagemCod[i - 1] == 'p' && (mensagemCod[i + 1] == 'p' || mensagemCod[i + 1] == ' '))  /*Satizfaz a decodificação para qualquer caracter não Vazio*/
             {
-                decodificado[j] = decodificacao[i];
+                decodificado[j] = mensagemCod[i];
                 j++;
             }
-            else if (decodificacao[i] == ' ') /*Trata o caso do caracter vazio*/
+            else if (mensagemCod[i] == ' ') /*Trata o caso do caracter vazio*/
             {
                 decodificado[j] = ' ';
                 j++;
             }
             
         }
-        decodificado[j] = decodificacao[mensagemCod.Length - 1]; /*A ultima posição da string sempre será uma letra que precisa ser incluida*/
-        decodificado[0] = char.ToUpper(decodificado[0]);
+        decodificado[j] = mensagemCod[mensagemCod.Length - 1]; /*A ultima posição da string sempre será uma letra que precisa ser incluida*/
         
         decodificado = decodificado[..(j+1)]; /*Modifica o vetor para que fique sem espaços vazios, j carrega o tamanho "real" da string decodificada (j+1 ocorre devido aos parametros do slicing)*/
+    
         string mensagemDec = new string(decodificado); /*Transforma o vetor de decodificação em uma nova string*/
         return mensagemDec;
     }
